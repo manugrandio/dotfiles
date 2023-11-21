126dd138033b4a1398e7589c678118d99a3518d7
Manu Grandio <manugrandio@fastmail.fm>
2023-11-21 22:09:19 +0100

    add bin scripts

---
+++ install-vim-plugins.sh
@@ -0,0 +1,14 @@
+# Install vim plugins and create sessions dir
+
+set -x
+
+mkdir -p ~/.vim/sessions
+
+# Pathogen plugins
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+git clone https://github.com/rizzatti/dash.vim.git ~/.vim/bundle/dash.vim
