32eb8a6711d189a23ab021f3e2168ec2a6df8502
Manu Grandio <manugrandio@fastmail.fm>
2023-11-21 23:22:05 +0100

    remaining config symlinking

--- setup-config-files.sh
+++ setup-config-files.sh
@@ -6,3 +6,5 @@ mkdir -p ~/.config
 [ ! -e ~/.config/git ] && ln -s "$PWD/git" ~/.config/git
 [ ! -e ~/.inputrc ] && ln -s "$PWD/other/inputrc" ~/.inputrc
 [ ! -e ~/.editrc ] && ln -s "$PWD/other/editrc" ~/.editrc
+[ ! -e ~/.tmux.conf ] && ln -s "$PWD/tmux/tmux.conf" ~/.tmux.conf
+[ ! -e ~/.vimrc ] && ln -s "$PWD/vim/vimrc" ~/.vimrc
