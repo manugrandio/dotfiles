8fbad712fbf54ca984408b84a5282927d502b358
Manu Grandio <manugrandio@fastmail.fm>
2023-11-25 19:19:32 +0100

    move vim sessions dir creation to vimrc

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -1,8 +1,4 @@
-# Install vim plugins and create sessions dir
-
-mkdir -p ~/.vim/sessions
-
-# Pathogen plugins
+# Install Pathogen plugins
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
