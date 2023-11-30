4c7b03e80384c3a1e4f28cb4fc3aaeb846e7d7d7
Manu Grandio <manugrandio@fastmail.fm>
2023-11-30 22:55:56 +0100

    bring back bash history

--- setup-config-files.sh
+++ setup-config-files.sh
@@ -3,6 +3,8 @@ mkdir -p ~/.config
 [ ! -e ~/.git-completion.bash ] && ln -s "$PWD/bin/git-completion.bash" ~/.git-completion.bash
 [ ! -e ~/.bashrc ] && ln -s "$PWD/bash/bashrc" ~/.bashrc
 [ ! -e ~/.bash_profile ] && ln -s "$PWD/bash/bash_profile" ~/.bash_profile
+# this brings back bash history:
+[ ! -e ~/.bash_sessions_disable ] && ln -s "$PWD/bash/bash_sessions_disable" ~/.bash_sessions_disable
 [ ! -e ~/.config/git ] && ln -s "$PWD/git" ~/.config/git
 [ ! -e ~/.inputrc ] && ln -s "$PWD/other/inputrc" ~/.inputrc
 [ ! -e ~/.editrc ] && ln -s "$PWD/other/editrc" ~/.editrc
