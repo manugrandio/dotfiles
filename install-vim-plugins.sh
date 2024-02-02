69600997fcce30859e4384a18ed73b2996bce07d
Manu Grandio <manugrandio@fastmail.fm>
2024-02-02 15:29:43 +0100

    simplify install vim plugins

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
 # Install Pathogen plugins
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
-git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
-git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
-git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
-git clone https://github.com/rizzatti/dash.vim.git ~/.vim/bundle/dash.vim
+pushd ~/.vim/bundle/ > /dev/null || exit
+git clone https://github.com/scrooloose/nerdcommenter.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/nvie/vim-flake8.git
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/rizzatti/dash.vim.git
+popd > /dev/null || exit
