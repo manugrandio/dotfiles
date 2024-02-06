46c4b7ad932c9aa549d053e5a3a3d652caea18b2
Manu Grandio <manugrandio@fastmail.fm>
2024-02-06 22:11:28 +0100

    add/remove vim plugins

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -4,9 +4,12 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 pushd ~/.vim/bundle/ > /dev/null || exit
-git clone https://github.com/scrooloose/nerdcommenter.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/nvie/vim-flake8.git
 git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/nvie/vim-flake8.git
 git clone https://github.com/rizzatti/dash.vim.git
+git clone https://github.com/tpope/vim-commentary.git
+git clone https://github.com/tpope/vim-dadbod.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/tpope/vim-rhubarb.git
+git clone https://github.com/tpope/vim-tbone.git
 popd > /dev/null || exit
